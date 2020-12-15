26f3a3b552b436056cf1dac9be22f86a2d1eaef9
Tolga Aksoy <tolgaaksoy@email.com>
2020-12-15 22:44:07 +0300

    swagger configured

--- src/main/java/com/tolgaaksoy/lokumcubaba/configuration/SwaggerConfiguration.java
+++ src/main/java/com/tolgaaksoy/lokumcubaba/configuration/SwaggerConfiguration.java
@@ -21,7 +21,7 @@ public class SwaggerConfiguration {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.tolgaaksoy.crudapp"))
+                .apis(RequestHandlerSelectors.basePackage("com.tolgaaksoy.lokumcubaba"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo())
                 .pathMapping("/");
