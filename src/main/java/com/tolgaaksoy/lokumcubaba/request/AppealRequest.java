c5d87f3e00c98bb0eeaec34530b8d9dcc03c8118
Tolga Aksoy <tolgaaksoy@email.com>
2020-12-15 22:30:41 +0300

    request,response created

---
+++ src/main/java/com/tolgaaksoy/lokumcubaba/request/AppealRequest.java
@@ -0,0 +1,9 @@
+package com.tolgaaksoy.lokumcubaba.request;
+
+import com.tolgaaksoy.lokumcubaba.dto.AppealDto;
+import lombok.Data;
+
+@Data
+public class AppealRequest {
+    AppealDto appealDto;
+}
---
+++ src/main/java/com/tolgaaksoy/lokumcubaba/response/AppealResponse.java
@@ -0,0 +1,9 @@
+package com.tolgaaksoy.lokumcubaba.response;
+
+import com.tolgaaksoy.lokumcubaba.dto.AppealDto;
+import lombok.Data;
+
+@Data
+public class AppealResponse {
+    private AppealDto appealDto;
+}
